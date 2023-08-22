4a68d1d36caf17c655c726c390dac66d01b4e683
Thiago Guedes <117332298+thiiigas@users.noreply.github.com>
2023-08-22 13:23:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+$('.navbar-collapse ul li a').click(function() {
+  $(".navbar-collapse").collapse('hide');});
 
+// jQuery for page scrolling feature
+$(function() {$('body').on('click', 'a.scrollable', function(event) {
+  var $anchor = $(this);
+  $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
+  event.preventDefault();
+  });
+});
